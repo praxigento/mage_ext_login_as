443dcf0fa7bebbb4b9d257f494b7c3cc8827b697
Alex Gusev <alex@flancer64.com>
2017-06-27 13:36:23 +0300

    LGAS-27 - Error 404 when "Admin routing compatibility mode for extensions" is Disabled

---
+++ src/app/code/community/Praxigento/LoginAs/Block/Adminhtml/Redirect.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+class Praxigento_LoginAs_Block_Adminhtml_Redirect
+    extends \Mage_Core_Block_Abstract
+{
+
+    protected function _toHtml()
+    {
+        /** @var $authPack Praxigento_LoginAs_Model_Package */
+        $authPack = Mage::getSingleton('prxgt_lgas_model/package');
+        $url = htmlentities($authPack->getRedirectUrl());
+        $formName = Praxigento_LoginAs_Config::REQ_PARAM_LAS_ID;
+        $packageId = htmlentities($authPack->getPackageId());
+        /** Post file name with login parameters */
+        $out = "
+<html>
+<body>
+<form action=\"$url\" method=\"post\" id=\"login-form\">
+    <input type=\"hidden\" name=\"$formName\" id=\"$formName\" value=\"$packageId\"/>
+</form>
+<script type=\"text/javascript\">
+    //<![CDATA[
+    document.getElementById(\"login-form\").submit();
+    //]]>
+</script>
+</body>
+</html>";
+        return $out;
+    }
+
+}
